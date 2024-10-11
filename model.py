91806c2707765b7176d54fd14710296d519d6a4b
AanasAshfaq <anasashfaq888@gmail.com>
2024-10-11 19:57:02 +0500

    new commit

--- model.py
+++ model.py
@@ -5,11 +5,11 @@ from io import BytesIO
 import torch
 from IPython.display import display
 
-# Check if GPU is available and set the device accordingly
-# device = 0 if torch.cuda.is_available() else -1
+#Check if GPU is available and set the device accordingly
+device = 0 if torch.cuda.is_available() else -1
 
-# # Load the model pipeline and pass the device argument
-# pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True, device=device)
+# Load the model pipeline and pass the device argument
+pipe = pipeline("image-segmentation", model="briaai/RMBG-1.4", trust_remote_code=True, device=device)
 
 # # Example image URL
 # #image_url = "https://media.newyorker.com/photos/62c4511e47222e61f46c2daa/4:3/w_2663,h_1997,c_limit/shouts-animals-watch-baby-hemingway.jpg"
@@ -21,8 +21,7 @@ from IPython.display import display
 # img = Image.open(BytesIO(response.content))
 
 # # Perform background removal and return the mask and the processed image
-# pillow_mask = pipe(image_url, return_mask=True)  # Get the mask
-# pillow_image = pipe(image_url)  # Get the masked image
+
 
 # # Display the original mask and the processed image
 # print("Displaying the mask (background removed):")
@@ -35,7 +34,10 @@ from IPython.display import display
 
 def process_image(file):
     img = Image.open(file.stream)  # Open the image
-    # Here you can perform any processing on the image
-    img.show()  # This will display the image
+    pillow_mask = pipe(img, return_mask=True)  # Get the mask
+    pillow_image = pipe(img)  # Get the masked image
+
+    # Here you can  perform any processing on the image
+    pillow_image.show()  # This will display the image
     # You might want to save or modify the image
     return {"message": "Image processed successfully!"}  # Return a response
